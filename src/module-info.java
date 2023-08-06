50678b62f3a9028fb321ec01f8834a25a4cae138
Adrian <86854740+adrianvla@users.noreply.github.com>
2023-08-06 16:19:00 +0200

    Delete src directory

--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-module spigotplugin {
-	requires spigot.api;
-	requires java.logging;
-	requires javax.websocket.api;
-	requires java.management;
-	requires jdk.httpserver;
-	requires jdk.management;
-}
